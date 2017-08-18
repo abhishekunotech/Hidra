eda45e1f6448abeafa3ec95e187206447be28a58
Abhishek Kulkarni <abhishek.kulkarni@unotechsoft.com>
2017-08-18 10:41:50 +0000

    Added Readme and some code to main file

--- v1/main.go
+++ v1/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
-func main(){
-    fmt.Println("Hello")
+func main() {
+
+	router := NewRouter()
+
+	go log.Fatal(http.ListenAndServeTLS(":443", "server.crt", "server.key", router))
 }
