5432d972f46a379aa91443257c5a106106816ea1
Abhishek Kulkarni <abhishek.kulkarni@unotechsoft.com>
2017-08-24 16:54:20 +0530

    Added getlinkedtickets.go (#29)

--- handlers/getlinkedtickets.go
+++ handlers/getlinkedtickets.go
@@ -1,4 +1,3 @@
-
  package handlers
 
 import (
@@ -9,37 +8,6 @@ import (
 	"io"
 //	"reflect"
 )
-/*
-type SessionObject struct{
-	SessionIDStrg	string	`json:"SessionID"`
-}
-
-func callSessionDetails(username string, password string) string{
-	url := "http://192.168.2.90:8080/felicity/nph-genericinterface.pl/Webservice/SessionAPI/SessionCreate?UserLogin="+username+"&Password="+password
-	fmt.Printf("\n\n url is "+url+"\n\n")
-	client := &http.Client{}
-	var bodyReader io.Reader
-	req, err := http.NewRequest("GET", url, bodyReader)
-	resp, err := client.Do(req)
-	if err != nil{
-		fmt.Printf("\n\n Session Creation failed because - \n\n")
-		fmt.Printf(err.Error())
-	}
-	req.Close = true
-	bodyText, err := ioutil.ReadAll(resp.Body)
-	var data SessionObject
-	err = json.Unmarshal(bodyText, &data)
-	fmt.Printf("\n\n body is ")
-	fmt.Printf(reflect.TypeOf(data).String())
-	fmt.Printf("\n\n")
-	if err != nil{
-		fmt.Printf("\n\n Error Occured in unmarshalling Session JSON \n\n")
-		fmt.Printf(err.Error())
-	}
-	fmt.Printf("\n\nSESSION ID IN CALL SESSION DETAILS IS "+data.SessionIDStrg+"\n\n")
-	return data.SessionIDStrg	
-}
-*/
 
 func callLinkedTickets(w http.ResponseWriter, r *http.Request, username string, password string, ticketid string){
 	
@@ -97,4 +65,3 @@ func GetLinkedTickets(w http.ResponseWriter, r *http.Request) {
 	callLinkedTickets(w,r,userName, password, ticketid)
 
 }
-
---
+++ handlers/routes.go
@@ -0,0 +1,104 @@
+
+ uckage Routes defines properties of an HTTP endpoint.
+	At runtime, the router will associate each Route with a http.Handler object, and use the Route properties to determine which Handler should be invoked.
+	Basically Routes will define routes for the different functions.
+	Install using go install in this directory.
+	Author: Operations Management Team - Unotech Software.
+*/
+package routes
+
+import (
+	"github.com/Unotechsoftware/Hydra/handlers"
+	"github.com/Unotechsoftware/Hydra/utils"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+type Route struct {
+	// Name is a key specifying which HTTP handler the router should associate
+	Name string
+	//Method is any valid HTTP method
+	Method string
+	//Pattern contains a path pattern
+	Pattern string
+	//handler
+	HandlerFunc http.HandlerFunc
+}
+
+//Routes is a Route collection.
+type Routes []Route
+
+func NewRouter() *mux.Router {
+	//Create a new mux router for given handler
+	router := mux.NewRouter().StrictSlash(true)
+	for _, route := range routes {
+
+		//Create the Route for the requested method,name,pattern and handler.
+
+		var handler http.Handler
+		handler = route.HandlerFunc
+		handler = utils.Logger(handler, route.Name)
+
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(handler)
+	}
+
+	return router
+}
+
+//Create different routes for required functions using name, method, path pattern and handler.
+var routes = Routes{
+	Route{
+		"Index",
+		"GET",
+		"/",
+		handlers.Index,
+	},
+	Route{
+		"getTicketDetails",
+		"GET",
+		"/getTicketDetails",
+		handlers.GetTicketDetails,
+	},
+	Route{
+		"getCILogs",
+		"GET",
+		"/getCILogs",
+		handlers.GetCILogs,
+	},
+	Route{
+		"getCIJobs",
+		"GET",
+		"/getCIJobs",
+		handlers.GetCIJobs,
+	},
+	Route{
+		"getCIDetails",
+		"GET",
+		"/getCIDetails",
+		handlers.GetCIDetails,
+	},
+	Route{
+		"Ticketcreate",
+		"POST",
+		"/Ticketcreate",
+		handlers.Ticketcreate,
+	},
+	Route{
+		"getLinkedChange",
+		"GET",
+		"/getLinkedChange",
+		handlers.GetLinkedChange,
+	},
+	Route{
+                "getLinkedTicket",
+                "GET",
+                "/getLinkedTicket",
+                handlers.GetLinkedTicket,
+        },
+
+}
+
