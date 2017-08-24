f0912362a00a98af9d14b4e9aa267f5a10b15af5
snehalkalke5 <snehal.kalke@unotechsoft.com>
2017-08-24 10:39:58 +0000

    Added getlinkedtickets.go

---
+++ handlers/getlinkedtickets.go
@@ -0,0 +1,100 @@
+
+ package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"io/ioutil"
+	"io"
+//	"reflect"
+)
+/*
+type SessionObject struct{
+	SessionIDStrg	string	`json:"SessionID"`
+}
+
+func callSessionDetails(username string, password string) string{
+	url := "http://192.168.2.90:8080/felicity/nph-genericinterface.pl/Webservice/SessionAPI/SessionCreate?UserLogin="+username+"&Password="+password
+	fmt.Printf("\n\n url is "+url+"\n\n")
+	client := &http.Client{}
+	var bodyReader io.Reader
+	req, err := http.NewRequest("GET", url, bodyReader)
+	resp, err := client.Do(req)
+	if err != nil{
+		fmt.Printf("\n\n Session Creation failed because - \n\n")
+		fmt.Printf(err.Error())
+	}
+	req.Close = true
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	var data SessionObject
+	err = json.Unmarshal(bodyText, &data)
+	fmt.Printf("\n\n body is ")
+	fmt.Printf(reflect.TypeOf(data).String())
+	fmt.Printf("\n\n")
+	if err != nil{
+		fmt.Printf("\n\n Error Occured in unmarshalling Session JSON \n\n")
+		fmt.Printf(err.Error())
+	}
+	fmt.Printf("\n\nSESSION ID IN CALL SESSION DETAILS IS "+data.SessionIDStrg+"\n\n")
+	return data.SessionIDStrg	
+}
+*/
+
+func callLinkedTickets(w http.ResponseWriter, r *http.Request, username string, password string, ticketid string){
+	
+	sessionIDString := callSessionDetails(username,password)
+	fmt.Printf("Session ID in call linked Changes is "+sessionIDString)
+	url := "http://192.168.2.90:8080/felicity/nph-genericinterface.pl/Webservice/TicketAPI/ListOfLinkedTickets?TicketID="+ticketid+"&SessionID="+sessionIDString
+	client := &http.Client{}
+	var bodyReader io.Reader
+    	req, err := http.NewRequest("GET", url,bodyReader)
+    	//req.SetBasicAuth(username,password)
+    	//req.Header.Set("Authorization", "Basic Z2xwaTpnbHBp")
+    	resp, err := client.Do(req)
+//	req.Close = true
+    	if err != nil{
+		fmt.Printf("\n\nThis caused the following error \n\n")
+        	fmt.Printf(err.Error())
+    	}
+	req.Close = true
+    	bodyText, err := ioutil.ReadAll(resp.Body)
+	var data interface{}
+    	err = json.Unmarshal(bodyText, &data)
+   	if err != nil {
+        	fmt.Printf(err.Error())
+    	}
+    	json.NewEncoder(w).Encode(data)		
+	/*json.NewEncoder(w).Encode(data)*/
+
+}
+
+//Function to get the details about ticket.
+
+func GetLinkedTickets(w http.ResponseWriter, r *http.Request) {
+	//body, _ := ioutil.ReadAll(r.Body)
+	mapHttp := r.URL.Query()
+	var userName string
+	var password string
+	var ticketid string
+	for key,value := range mapHttp {
+		if key == "ticketID"{
+			for _, valueStrg := range value {
+				ticketid = valueStrg
+			}
+		}
+		if key == "username"{
+			for _, valueStrg := range value {
+				userName = valueStrg
+			}
+		}
+		if key == "password"{
+			for _, valueStrg := range value {
+				password = valueStrg
+			}
+		}
+	}
+	callLinkedTickets(w,r,userName, password, ticketid)
+
+}
+
