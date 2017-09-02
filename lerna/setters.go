f511f9d2dfe2cf020cb432d2b9fe5f5d530692c4
Abhishek Kulkarni <abhishek.kulkarni@unotechsoft.com>
2017-09-02 06:29:59 +0000

    Added Setters

---
+++ lerna/setters.go
@@ -0,0 +1,75 @@
+
+func SetComponentType_Version(){
+
+}
+
+func SetComponentType_Url(){
+
+
+}
+
+
+func SetComponentType_Apis(){
+
+}
+
+func SetAPIType_Version(){
+
+
+}
+
+
+func SetAPIType_Uri(){
+
+}
+
+func SetAPIType_Parameter(){
+
+
+}
+
+
+func SetRouteType_Version(){
+
+}
+
+
+func SetRouteType_Method(){
+
+}
+
+func SetRouteType_Uri(){
+
+
+}
+
+
+func SetRouteType_Component(){
+
+
+
+}
+
+
+func SetRouteType_Handler(){
+
+
+}
+
+
+func SetJSONObjectType_Version(){
+
+
+}
+
+
+func SetJSONObjectType_Routes(){
+
+
+}
+
+
+func SetJSONObjectType_Components(){
+
+
+}
