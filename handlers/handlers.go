80dea81997232bec62276d3456315ac6977bebfd
Abhishek Kulkarni <abhishek.kulkarni@unotechsoft.com>
2017-09-04 05:50:46 +0000

    Removed Index commented code from handlers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -185,38 +185,6 @@ func creatorOfTickets(jsonInput string) string {
 	fmt.Println("response Body:", string(body))
 	return string(body)
 }
-/*
-func Index(w http.ResponseWriter, r *http.Request) {
-	//fmt.Fprintln(w, "hello bla")
-	var ticketid string 
-	var username string
-	var password string
-	ticketid = "627"
-	username = "amol"
-	password = "amol"
-	
-	url := "http://192.168.2.152/felicity/nph-genericinterface.pl/Webservice/TicketAPI/TicketGet/"+ticketid+"?UserLogin="+username+"&Password="+password
-	
-	client := &http.Client{}
-	var bodyReader io.Reader
-    	req, err := http.NewRequest("GET", url,bodyReader)
-    	//req.SetBasicAuth(username,password)
-    	//req.Header.Set("Authorization", "Basic Z2xwaTpnbHBp")
-    	resp, err := client.Do(req)
-//	req.Close = true
-    	if err != nil{
-		logger.Error("\n\nThis caused the following error \n\n")
-        	logger.Error(err.Error())
-    	}
-	req.Close = true
-    	bodyText, err := ioutil.ReadAll(resp.Body)
-	var data interface{}
-    	err = json.Unmarshal(bodyText, &data)
-   	if err != nil {
-        	logger.Error(err.Error())
-    	}
-}
-*/
 //Function to get CI logs.
 func GetCILogs(w http.ResponseWriter, r *http.Request) {
 
