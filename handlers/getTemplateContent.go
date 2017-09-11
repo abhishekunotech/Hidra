86c4f3cf8fd2ab204e8fb02da1a92f9f08d1ad5f
Abhishek Kulkarni <abhishek.kulkarni@unotechsoft.com>
2017-09-11 14:24:17 +0000

    Completed Template Content API

---
+++ handlers/getTemplateContent.go
@@ -0,0 +1,76 @@
+package handlers
+
+import (
+	"github.com/antigloss/go/logger"
+        "encoding/json"
+	"github.com/Unotechsoftware/Hydra/lerna"
+        "net/http"
+	"fmt"
+        "io/ioutil"
+	//"reflect"
+        //"io"
+)
+func callContentTemplate(w http.ResponseWriter, r *http.Request, username string, password string, templateID string){
+
+	sessionIDString := callSessionDetails(username,password)
+
+	fmt.Println("session id is ::",sessionIDString)        
+	ConfObj := lerna.ReturnConfigObject()
+	felicitybaseurl := ConfObj.Sub("components.otrs").GetString("url")
+	fmt.Println("base url:- ",felicitybaseurl)
+	felicityapiuri := ConfObj.Sub("components.otrs.apis.templatecontent").GetString("uri")
+		
+	url := felicitybaseurl+felicityapiuri+"?SessionID="+sessionIDString+"&TemplateID="+templateID
+
+	//fmt.Println("url is::",url)	
+	res, err:= http.Get(url)
+	if err != nil{
+		logger.Error(err.Error())	
+	}
+       
+	bodyText, err := ioutil.ReadAll(res.Body)
+	
+        var data interface{}
+        err = json.Unmarshal(bodyText, &data)
+        if err != nil {
+                logger.Error(err.Error())
+        }
+	w.Header().Set("Content-Type", "application/json")
+        json.NewEncoder(w).Encode(data)
+        /*json.NewEncoder(w).Encode(data)*/
+
+}
+
+//Function to get list of work orders
+// Request as http://ip-host/getListOfWorkOrders?ticketID=521&password=abhik&userLogin=abhik
+
+func (h *Handler) GetTemplateContent(w http.ResponseWriter, r *http.Request) {
+        //body, _ := ioutil.ReadAll(r.Body)
+
+	mapHttp := r.URL.Query()
+        var userName string
+        var password string
+	var templateID string
+        for key,value := range mapHttp {
+                if key == "userLogin"{
+                        for _, valueStrg := range value {
+                                userName = valueStrg
+                        }
+                }
+                if key == "password"{
+                        for _, valueStrg := range value {
+                                password = valueStrg
+                        }
+                }
+		if key == "TemplateID"{
+			for _,valueStrg := range value {
+				templateID = valueStrg
+			}
+		}
+        }
+
+        callContentTemplate(w,r,userName, password, templateID)
+
+}
+
+
