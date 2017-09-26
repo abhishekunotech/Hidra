7680486b04ba99eb2dcb51ab549982cc300da73c
Abhishek Kulkarni <abhishek.kulkarni@unotechsoft.com>
2017-09-26 11:08:49 +0000

    Added Method to API LIST

--- models/apiobject.go
+++ models/apiobject.go
@@ -3,4 +3,5 @@ package models
 type APIObject struct{
 	Name	string	`json:"Name"`
 	URI	string	`json:"URI"`
+	Method	string	`json:"Method"`
 }
--- utils/listapis.go
+++ utils/listapis.go
@@ -13,6 +13,7 @@ func ListRoutes() []models.APIObject{
         for _, routeVal := range RouteKeyArray {
                 var tempRouteObj models.APIObject
                 tempRouteObj.Name = routeVal
+		tempRouteObj.Method = ConfObj.GetString("routes." + routeVal + ".method")
                 tempRouteObj.URI = ConfObj.GetString("routes." + routeVal + ".URI")
 
                 routes = append(routes, tempRouteObj)
